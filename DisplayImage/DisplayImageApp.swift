36bb9b08a63c8ff39a92071ed3722eae8ddeabff
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-07-27 12:43:50 +0800

    Initial Commit

---
+++ DisplayImage/DisplayImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayImageApp.swift
+//  DisplayImage
+//
+//  Created by Neil Francis Hipona on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DisplayImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
