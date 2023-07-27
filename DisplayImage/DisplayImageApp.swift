813cbe0475ffb8be2cbe31a12438a5e50de7c737
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-07-27 17:59:01 +0800

    Add entitlements and other unstagged files

--- DisplayImage/DisplayImageApp.swift
+++ DisplayImage/DisplayImageApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DisplayImageApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ImageView(coordinator: .init())
         }
     }
 }
